16c3dee09615274a063a540ce9628391795b8786
Vitaly Borisov <vitaliy.borisov@intspirit.com>
2021-10-14 16:19:28 +0300

    added animation

--- src/App.js
+++ src/App.js
@@ -1,28 +1,43 @@
 /* eslint-disable function-paren-newline */
 /* eslint-disable implicit-arrow-linebreak */
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import PostService from './API/PostService';
 import PostFilter from './components/PostFilter.jsx';
 import PostForm from './components/PostForm.jsx';
 import PostList from './components/PostList.jsx';
 import MyButton from './components/UI/button/MyButton.jsx';
+import Loader from './components/UI/Loader/Loader.jsx';
 import MyModal from './components/UI/MyModal/MyModal.jsx';
 import { usePosts } from './hooks/usePosts';
 import './styles/App.css';
 
 function App() {
-  const [posts, setPosts] = useState([
-    { id: 1, title: 'JS8', body: 'Description' },
-    { id: 2, title: 'jS2', body: 'Description4' },
-    { id: 3, title: 'JS3', body: 'Description1' },
-  ]);
+  const [posts, setPosts] = useState([]);
   const [filter, setFilter] = useState({ sort: '', query: '' });
   const [modal, setModal] = useState(false);
   const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
+  const [isPostLoading, setIsPostLoading] = useState(false);
+
+  useEffect(() => {
+    // eslint-disable-next-line no-use-before-define
+    fetchPosts();
+  }, []);
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost]);
     setModal(false);
   };
+
+  async function fetchPosts() {
+    setIsPostLoading(true);
+    // eslint-disable-next-line no-shadow
+    setTimeout(async () => {
+      // eslint-disable-next-line no-shadow
+      const posts = await PostService.getAll();
+      setPosts(posts);
+      setIsPostLoading(false);
+    }, 1000);
+  }
 
   const removePost = (post) => {
     setPosts(posts.filter((p) => p.id !== post.id));
@@ -38,7 +53,23 @@ function App() {
       </MyModal>
       <hr style={{ margin: '15px 0' }} />
       <PostFilter filter={filter} setFilter={setFilter} />
-      <PostList remove={removePost} posts={sortedAndSearchedPosts} title="JS" />
+      {isPostLoading ? (
+        <div
+          style={{
+            display: 'flex',
+            justifyContent: 'center',
+            marginTop: '50px',
+          }}
+        >
+          <Loader />
+        </div>
+      ) : (
+        <PostList
+          remove={removePost}
+          posts={sortedAndSearchedPosts}
+          title="JS"
+        />
+      )}
     </div>
   );
 }
