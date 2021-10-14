926cbe20a0855f91cd98791d731cbd1872262fab
Vitaly Borisov <vitaliy.borisov@intspirit.com>
2021-10-14 15:31:43 +0300

    added hooks

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 /* eslint-disable function-paren-newline */
 /* eslint-disable implicit-arrow-linebreak */
-import React, { useMemo, useState } from 'react';
+import React, { useState } from 'react';
 import PostFilter from './components/PostFilter.jsx';
 import PostForm from './components/PostForm.jsx';
 import PostList from './components/PostList.jsx';
 import MyButton from './components/UI/button/MyButton.jsx';
 import MyModal from './components/UI/MyModal/MyModal.jsx';
+import { usePosts } from './hooks/usePosts';
 import './styles/App.css';
 
 function App() {
@@ -16,23 +17,7 @@ function App() {
   ]);
   const [filter, setFilter] = useState({ sort: '', query: '' });
   const [modal, setModal] = useState(false);
-
-  const sortedPosts = useMemo(() => {
-    if (filter.sort) {
-      return [...posts].sort((a, b) =>
-        a[filter.sort].localeCompare(b[filter.sort]),
-      );
-    }
-    return posts;
-  }, [filter.sort, posts]);
-
-  const sortedAndSearchedPosts = useMemo(
-    () =>
-      sortedPosts.filter((post) =>
-        post.title.toLowerCase().includes(filter.query.toLowerCase()),
-      ),
-    [filter.query, sortedPosts],
-  );
+  const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost]);
