e9ae32328388b4f0db15a588360ab7a706e4a40a
Vitaly Borisov <vitaliy.borisov@intspirit.com>
2021-10-15 11:06:27 +0300

    added comments to the post

--- src/pages/PostIdPage.jsx
+++ src/pages/PostIdPage.jsx
@@ -8,13 +8,19 @@ import { useFetching } from '../hooks/useFetching';
 const PostIdPage = () => {
   const params = useParams();
   const [post, setPost] = useState({});
+  const [comments, setComments] = useState([]);
   const [fetchPostById, isLoading, error] = useFetching(async (id) => {
     const response = await PostService.getById(id);
     setPost(response.data);
   });
+  const [fetchComments, isComLoading, comError] = useFetching(async (id) => {
+    const response = await PostService.getCommentsByPostId(id);
+    setComments(response.data);
+  });
 
   useEffect(() => {
     fetchPostById(params.id);
+    fetchComments(params.id);
   }, []);
 
   return (
@@ -24,6 +30,19 @@ const PostIdPage = () => {
       ) : (
         <div>
           {post.id}. {post.title}
+        </div>
+      )}
+      <h2>Comments</h2>
+      {isComLoading ? (
+        <Loader></Loader>
+      ) : (
+        <div>
+          {comments.map((comm) => (
+            <div key={comm.id} style={{ marginTop: '15px' }}>
+              <h5>{comm.email}</h5>
+              <div>{comm.body}</div>
+            </div>
+          ))}
         </div>
       )}
     </div>
